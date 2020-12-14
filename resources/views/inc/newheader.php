9700a7c82941aa2e80bd98d5cbed91b6d3b125cf
alexkirillof <67540989+alexkirillof@users.noreply.github.com>
2020-12-14 14:29:51 +0300

    Add files via upload

---
+++ resources/views/inc/newheader.php
@@ -0,0 +1,9 @@
+<header class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+  <p class="h5 my-0 me-md-auto fw-normal">My site</p>
+  <nav class="my-5 my-md-0 me-md-5 my-nav">
+    <a class="p-2 text-dark" href="/Main">Main</a>
+    <a class="p-2 text-dark" href="/Politics">Politics</a>
+    <a class="p-2 text-dark" href="/Society">Society</a>
+    <a class="p-2 text-dark" href="/Sport">Sport</a>
+  </nav>
+</header>
