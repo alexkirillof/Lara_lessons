8664767ce68862376782e8857c42d99c0dbd7abf
alexkirillof <67540989+alexkirillof@users.noreply.github.com>
2020-12-14 14:22:26 +0300

    Add files via upload

---
+++ app/Http/Controllers/AddNewsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AddNewsController extends Controller
+{
+    public function AddNews() 
+    {
+     return view('AddNews');
+    }
+ }
