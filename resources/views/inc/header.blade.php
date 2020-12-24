0002c234513c97038c23c1bd2b35d9d11c9abeca
alexkirillof <67540989+alexkirillof@users.noreply.github.com>
2020-12-24 08:20:42 +0300

    Delete header.blade.php

--- resources/views/inc/header.blade.php
+++
@@ -1,9 +0,0 @@
-<header class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-  <p class="h5 my-0 me-md-auto fw-normal">My site</p>
-  <nav class="my-5 my-md-0 me-md-5 my-nav">
-    <a class="p-2 text-dark" href="/">Welcome</a>
-    <a class="p-2 text-dark" href="/NewsCat">NewsCat</a>
-    <a class="p-2 text-dark" href="/login">Login</a>
-    <a class="p-2 text-dark" href="/AddNews">AddNews</a>
-  </nav>
-</header>
