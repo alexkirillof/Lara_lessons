e4bc334f09b3b4116e8133b55248b3e009c3e716
alexkirillof <67540989+alexkirillof@users.noreply.github.com>
2021-01-13 08:33:46 +0300

    Add files via upload

---
+++ database/migrations/2021_01_12_085513_create_news_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50)
+            ->unique()
+            ->nullable(false);
+        $table->text('description');
+        $table->boolean('actual')
+            ->default(false);
+        $table->string('source', 100);
+        $table->dateTime('publish_date');
+       
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
