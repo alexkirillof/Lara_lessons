c0e7e2bf5d87d2f4093158c8c5071ac65e9ceae7
alexkirillof <67540989+alexkirillof@users.noreply.github.com>
2021-01-13 08:27:30 +0300

    Add files via upload

---
+++ app/Models/Category.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    public function news()
+    {
+        return $this->hasMany(News::class);
+    }
+
+    public function getCategories()
+    {
+        return  [
+            1 => 'Main',
+            2 => 'Politics',
+            3 => 'Society',
+            4 => 'Sport'
+        ];
+    }
+}
---
+++ app/Models/News.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\News
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $description
+ * @property int $active
+ * @property string $source
+ * @property string $publish_date
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $deleted_at
+ * @method static \Illuminate\Database\Eloquent\Builder|News newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|News newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|News query()
+ * @method static \Illuminate\Database\Eloquent\Builder|News whereActive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|News whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|News whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|News whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|News whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|News wherePublishDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|News whereSource($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|News whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|News whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class News extends Model
+{
+    use HasFactory;
+
+    protected $table = 'news';
+
+    protected $fillable = [
+        'id',
+        'title',
+        'description',
+        'source',
+        'publish_date',
+        'category_id'
+    ];
+
+   
+
+    public function getByCategoryId(int $categoryId)
+    {
+        return static::query()
+            ->where('category_id', $categoryId)
+            ->get();
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
