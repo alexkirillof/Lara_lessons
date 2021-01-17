8a3e536702a0857b8c9c154fc31e4a0300758b52
alexkirillof <67540989+alexkirillof@users.noreply.github.com>
2021-01-17 21:02:40 +0300

    Add files via upload

---
+++ resources/lang/ru/adminlang.php
@@ -0,0 +1,11 @@
+<?php
+
+
+return [
+    'news_title' => 'заголовок',
+    'news_description' => 'значение',
+    'news_category' => 'категория',
+    'news_publish_date' => 'дата',
+
+
+];
--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -1,6 +1,3 @@
-
-
-
 @php
 /** @var \App\Models\News $model */
 /** @var \App\Models\Category[] $categories */
@@ -8,66 +5,66 @@
 @extends('layouts.app')
 
 @section('title')
-    Админка новости
+Админка новости
 @endsection
 
 @section('content')
-    <div class="row justify-content-center">
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
+<div class="row justify-content-center">
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
 
     @if (session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
-        @endif
-        <div class="col-md-6">
-            <h1>Create news</h1>
-            {!! Form::open(['route' => 'admin::news::save']) !!}
-                @if($model->id)
-                <input type="hidden" name="id" value="{{$model->id}}">
-                @endif
-                <div class="form-group">
-                    <label>name</label>
-                    {!! Form::text("title",$model->title ?? old('title'), ['class' => "form-control"]) !!}
-                    @error('title')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="form-group">
-                    <label>Description</label>
-                    {!! Form::textarea("description",$model->description ?? old('content') ??"", ['class' => "form-control"]) !!}
-                </div>
-                <div class="form-group">
-                    <label>Category</label>
-                    {!! Form::select('category_id', $categories, $model->category_id, ['class' => 'form-control']) !!}
-                </div>
-                <div class="form-group">
-                    <input type="hidden" name="active" value="0">
-                    <label>
-                        {!! Form::checkbox("active",1, $model->active) !!}
-                        Active
-                    </label>
-                </div>
-                <div class="form-group">
-                    <label>Publish date</label>
-                    {!! Form::date(
-                            'publish_date',
-                            $model->publish_date ?? old('publish_date'),
-                            ['dataformatas' =>'Y-m-d', 'class' => 'form-control'] )
-                    !!}
-                </div>
-                <div class="form-group">
-                    <input class="btn btn-success" type="submit" value="Save">
-                </div>
-            {!! Form::close() !!}
-        </div>
+    <div class="alert alert-success">
+        {{ session('success') }}
     </div>
+    @endif
+    <div class="col-md-6">
+        <h1>Create news</h1>
+        {!! Form::open(['route' => 'admin::news::save']) !!}
+        @if($model->id)
+        <input type="hidden" name="id" value="{{$model->id}}">
+        @endif
+        <div class="form-group">
+            <label>{{__('adminlang.news_title')}}</label>
+            {!! Form::text("title",$model->title ?? old('title'), ['class' => "form-control"]) !!}
+            @error('title')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="form-group">
+            <label>{{__('adminlang.news_description')}}</label>
+            {!! Form::textarea("description",$model->description ?? old('content') ??"", ['class' => "form-control"]) !!}
+        </div>
+        <div class="form-group">
+            <label>{{__('adminlang.news_category')}}</label>
+            {!! Form::select('category_id', $categories, $model->category_id, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
+            <input type="hidden" name="active" value="0">
+            <label>
+                {!! Form::checkbox("active",1, $model->active) !!}
+                Active
+            </label>
+        </div>
+        <div class="form-group">
+            <label>{{__('adminlang.news_publish_date')}}</label>
+            {!! Form::date(
+            'publish_date',
+            $model->publish_date ?? old('publish_date'),
+            ['dataformatas' =>'Y-m-d', 'class' => 'form-control'] )
+            !!}
+        </div>
+        <div class="form-group">
+            <input class="btn btn-success" type="submit" value="Save">
+        </div>
+        {!! Form::close() !!}
+    </div>
+</div>
 @endsection
